d432131a5e41f33c8a856ef745e5199c3889bbc3
felipinaseosd <130668980+felipinaseosd@users.noreply.github.com>
2023-09-29 08:48:58 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// Definindo as variáveis
+$valor1 = 5;
+$valor2 = 2;
+$nome1 = "Robson";
+$nome2 = "Pedro";
+$valor3 = 12;
+$valor4 = 11;
+
+// Verificando se 5 é maior que 2
+if ($valor1 > $valor2) {
+    echo "5 é maior que 2<br>";
+}
+
+// Verificando se Robson é diferente de Pedro
+if ($nome1 != $nome2) {
+    echo "Robson é diferente de Pedro<br>";
+}
+
+// Verificando se 12 é menor ou igual a 11
+if ($valor3 <= $valor4) {
+    echo "12 é menor ou igual a 11<br>";
+}
+?>
